a1cd3c73f33c5575231a464827666ce90373694b
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2024-07-13 12:51:45 +0600

    crud firestore code needs test

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from "react";
 // firebase imports
 import {
   getAuth,
+  createUserWithEmailAndPassword,
+  getAuth,
   GoogleAuthProvider,
   signInWithPopup,
   signOut,
+  signInWithEmailAndPassword,
 } from "firebase/auth";
 // import { app } from "../Firebase/firebase.init.js";
 import { app } from "../firebase/firebase.init";
@@ -14,13 +17,12 @@ const googleProvider = new GoogleAuthProvider();
 const useFirebase = () => {
   const auth = getAuth(app);
 
-  https://youtu.be/woITklcvthg?t=1419
-
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true); // for loadingSpinner
 
-  const handleGoogleSignIn = () => {
-    signInWithPopup(auth, googleProvider)
+  // GOOGLE SIGN IN
+  const handleGoogleSignIn = async () => {
+    await signInWithPopup(auth, googleProvider)
       .then((result) => {
         // This gives you a Google Access Token. You can use it to access the Google API.
         const credential = googleProvider.credentialFromResult(result);
@@ -42,9 +44,40 @@ const useFirebase = () => {
         // ...
       });
   };
+
+  // EMAIL SIGNUP
+  const emailSignup = async (email, password) => {
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        setUser(user);
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  };
+
+  // EMAIL SIGN IN
+  const emailSignIn = async (email, password) => {
+    await signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        setUser(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  };
+
   /* 🔽⏬🔽⏬ SIGN OUT  🔽⏬🔽⏬ */
-  const logOut = () => {
-    signOut(auth)
+  const logOut = async () => {
+    await signOut(auth)
       .then((d) => {
         // Sign-out successful.
         console.log(d, "signed Out");
@@ -75,6 +108,8 @@ const useFirebase = () => {
     setLoading,
     logOut,
     handleGoogleSignIn,
+    emailSignup,
+    emailSignIn,
   };
 };
 
