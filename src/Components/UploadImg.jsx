8763156ff7814359146f659557466b119d3b8b82
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2024-08-21 19:15:53 +0600

    img upload component added

--- src/Components/UploadImg.jsx
+++ src/Components/UploadImg.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import { storage } from "../firebase/firebase.init";
-https://youtu.be/5986IgwaVKE?t=339
+import { ref, uploadBytes } from "firebase/storage";
+// import {v4} from "uuid"
+
+// https://youtu.be/5986IgwaVKE?t=339
 const UploadImg = () => {
-  return <div></div>;
+  const [img, setImg] = React.useState("");
+  const handleClick = () => {
+    const imgRef = ref(storage, `files/${v4()}`);
+    uploadBytes(imgRef, img);
+  };
+  return (
+    <div>
+      <input type="file" onChange={(e) => setImg(e.target.files[0])} />
+      <button onClick={handleClick}>Upload</button>
+    </div>
+  );
 };
 
 export default UploadImg;
